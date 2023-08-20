e142befb03a88e8f14b1ad111e67c82dc32186b7
Davitaridani <davitaridani@gmail.com>
2023-08-20 15:02:11 +0700

    add css module  and add data  static

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,6 @@
 import * as asset_about from "./about";
 import * as asset_home from "./home";
+import * as asset_guide from "./guide";
+import * as asset_product from "./product";
 
-export { asset_home, asset_about };
+export { asset_home, asset_about, asset_product, asset_guide };
---
+++ src/assets/images/product/index.js
@@ -0,0 +1,1 @@
+export { default as product_1 } from "./product-dummy.png";
